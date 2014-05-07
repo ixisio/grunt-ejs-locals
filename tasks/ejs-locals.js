8a16b24fbc417bc8e40c3dcac32784d0572fb360
ixisio <andy@interaction.io>
2014-05-07 12:17:39 +0200

    Initial Commit

---
+++ tasks/ejs-locals.js
@@ -0,0 +1,35 @@
+/*
+ * grunt-ejs-locals
+ * https://github.com/ixisio/grunt-ejs-locals
+ *
+ * Copyright (c) 2014 ixisio
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+  'use strict';
+
+  var ejs = require('ejs-locals');
+
+  grunt.registerMultiTask('ejs', 'compile ejs templates', function() {
+    var options = this.options();
+
+    grunt.verbose.writeflags(options, 'Options');
+
+    this.files.forEach(function(file) {
+
+      // Pass empty settings obj (like express)
+      // https://github.com/RandomEtc/ejs-locals/issues/32
+      options.settings = {};
+
+      // Generate EJS-Locals HTML
+      ejs(file.src[0], options, function(err, html) {
+        if (err) {
+          return grunt.fail.fatal('Err:  ' + err);
+        }
+        grunt.file.write(file.dest, html);
+        grunt.log.ok('Wrote ' + file.dest);
+      });
+    })
+  });
+};
